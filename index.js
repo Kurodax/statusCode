3d5a6aa96214f60e66e5cfbb44ab743332910673
Kurodax <l.kuroda@hotmail.com>
2020-01-07 16:00:27 -0300

    codes

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.post('/', function (req, res) {
-  res.send('Funcionou metodo POST 02');
+app.post('/', function(req, res, next) {
+  res.sendStatus(401);
 });
 
-app.get('/', function (req, res){
-  res.send('Testando medodo GET 01');
+app.get('/', function(req, res, next) {
+  res.sendStatus(500);
 });
 
-app.delete('/', function (req, res){
-  res.send('Medoto Deletado 00');
+app.put('/', function(req, res, next) {
+  res.sendStatus(501);
 });
 
-app.options('/', function (req, res){
-  res.send('Medoto OP');
+app.options('/', function(req, res, next) {
+  res.sendStatus(400);
 });
 
-app.patch('/', function (req, res){
-  res.send('Medoto PATCH testando');
-});
-
-app.head('/', function (req, res){
-  res.send('Medoto HEAD buscando o topo');
-});
-
-app.put('/', function (req, res){
-  res.send('Medoto PUT 0055');
+app.patch('/', function(req, res, next) {
+  res.sendStatus(504);
 });
 
 
-app.listen(5000, function () {
-  console.log('Servidor funcionando na porta 5000!');
+app.listen(3000, function () {
+  console.log('Servidor funcionando na porta 3000!');
 });
