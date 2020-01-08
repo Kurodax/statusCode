25e246a89488cce24b7bc82f1a1afdbcaa602fe6
Kurodax <l.kuroda@hotmail.com>
2020-01-08 15:39:25 -0300

    codes

--- index.js
+++ index.js
@@ -1,27 +1,54 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.post('/', function(req, res, next) {
+//                           ##### STATUS CODE #### //
+
+/*app.post('/', (req, res,) => {
+  res.status(401).json ({Status: Não autorizado});
+});
+
+app.post('/', (req, res,) => {
   res.sendStatus(401);
 });
 
-app.get('/', function(req, res, next) {
-  res.sendStatus(500);
+app.patch('/ola/:nome/:cargo', (req, res,) => {
+  res.status(500).json(Sstatus: Erro interno do servidor (Internal Server Error));
 });
 
-app.put('/', function(req, res, next) {
-  res.sendStatus(501);
+app.get('/ola/:nome/:cargo', (req, res,) => {
+  res.json({"Ola ": req.params.nome});
+}); */
+
+//                           ##### FIM STATUS CODE #### //
+
+
+
+               // #####  CONVERTER O BODY [req.body] DA REQUISIÇÃO PARA JSON  ##### //
+app.use(express.json())
+
+
+                              // #####  ARRAY  ##### //
+const usuario = [
+  {name: 'Henrique', email: 'henrique@hotmail.com'},
+  {name: 'Lucas', email: 'l.kuroda@hotmail.com'},
+  {name: 'Antarctica', email: 'antarctica@bol.com'}
+];
+
+                              // ##### MOSTRA O ARRAY #### //
+app.get('/dados', (req, res) => {
+  res.json({usuario});
 });
 
-app.options('/', function(req, res, next) {
-  res.sendStatus(400);
+                              // ##### INSERI DADOS NO ARRAY #### //
+app.post('/inserir', (req, res,) => {
+  usuario.push (req.body);
+  console.log(usuario);
+  res.json(req.body);
 });
 
-app.patch('/', function(req, res, next) {
-  res.sendStatus(504);
-});
 
 
-app.listen(3000, function () {
+
+app.listen(3000, () => {
   console.log('Servidor funcionando na porta 3000!');
 });
